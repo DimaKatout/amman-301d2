0631b130f0787d140f08772cd2b56215a4287b24
Ahmad Alawad <ahmad.alawad.sf@gmail.com>
2019-12-31 16:17:42 +0200

    Demo 12 is done

--- class-12/demo/todo-app/server.js
+++ class-12/demo/todo-app/server.js
@@ -1,82 +1,84 @@
 'use strict'
 
-// Application Dependencies
-const express = require('express');
-const pg = require('pg');
-
 // Environment variables
 require('dotenv').config();
+
+// Application Dependencies
+const express = require('express');
 
 // Application Setup
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Database connection
+const pg = require('pg');
+const client = new pg.Client(process.env.DATABASE_URL);
+
 // Express middleware
 // Utilize ExpressJS functionality to parse the body of the request
 app.use(express.urlencoded({extended: true}));
+
 // Specify a directory for static resources
 app.use(express.static('./public'));
-
-// Database Setup
-const client = new pg.Client(process.env.DATABASE_URL);
-client.connect();
-client.on('error', err => console.error(err));
 
 // Set the view engine for server-side templating
 app.set('view engine', 'ejs');
 
-// API Routes
-app.get('/', getTasks);
 
-app.get('/tasks/:task_id', getOneTask);
+function handleError(error, response){
+    response.render('pages/error', {error: error});
+}
 
-app.get('/add', showForm);
+// Routes 
 
-app.post('/add', addTask);
+app.get('/' ,getAllTodos);
+app.get('/add', addToDB);
+app.post('/add', processAdd);
+app.get('/tasks/:task_id', getSpecificTask);
+
+function getSpecificTask(req, res){
+    let id = req.params.task_id;
+    let SQL = `SELECT * FROM Todos WHERE id=$1`;
+    let values = [id];
+
+    client.query(SQL, values)
+    .then( data => {
+        console.log(data.rows[0]);
+        res.render('task', {task: data.rows[0]});
+    }).catch(err => handleError(err));
+}
+
+function getAllTodos(req, res){
+    let SQL = `SELECT * FROM Todos;`; 
+    client.query(SQL)
+    .then( data => {
+        res.render('index', {todos: data.rows});
+    }).catch(err => handleError(err));
+}
+
+function addToDB(req, res){
+    res.render('add');
+}
+
+function processAdd(req, res){
+    console.log(req.body);
+    let {title, description, category, contact, status} = req.body;
+    let SQL = "INSERT INTO Todos (title, description, contact, category, status) VALUES ($1, $2, $3, $4, $5);";
+    let values = [title, description, contact, category, status];
+    client.query(SQL, values)
+    .then( () => {
+        res.redirect('/');
+    }).catch(err => handleError(err));
+    
+}
 
 app.get('*', (req, res) => res.status(404).send('This route does not exist'));
 
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
 
-// HELPER FUNCTIONS
+client.connect()
+.then( () => {
+    app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+})
+.catch(err => handleError(err));
 
-function getTasks(request, response) {
-  let SQL = 'SELECT * from tasks;';
-
-  return client.query(SQL)
-    .then(results => response.render('index', {results: results.rows}))
-    .catch(handleError);
-}
-
-function getOneTask(request, response) {
-  let SQL = 'SELECT * FROM tasks WHERE id=$1;';
-  let values = [request.params.task_id];
-
-  return client.query(SQL, values)
-    .then(result => {
-      // console.log('single', result.rows[0]);
-      return response.render('pages/detail-view', {task: result.rows[0]});
-    })
-    .catch(err => handleError(err, response));
-}
-
-function showForm(request, response) {
-  response.render('pages/add-view');
-}
-
-function addTask(request, response) {
-  console.log(request.body);
-  let {title, description, category, contact, status} = request.body;
-
-  let SQL = 'INSERT INTO tasks(title, description, category, contact, status) VALUES ($1, $2, $3, $4, $5);';
-  let values = [title, description, category, contact, status];
-
-  return client.query(SQL, values)
-    .then(response.redirect('/'))
-    .catch(err => handleError(err, response));
-}
-
-function handleError(error, response) {
-  response.render('pages/error-view', {error: 'Uh Oh'});
-}
