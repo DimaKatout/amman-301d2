74dcc10b5c8c6fcb7c3dbe07ae2be93a1ab35443
Ahmad Alawad <ahmad.alawad.sf@gmail.com>
2020-01-05 12:38:41 +0200

    Uploaded Class13 Demo

--- class-13/demo/todo-app/server.js
+++ class-13/demo/todo-app/server.js
@@ -1,36 +1,30 @@
 'use strict'
 
+// Environment variables
+require('dotenv').config();
+
 // Application Dependencies
 const express = require('express');
 const pg = require('pg');
-const methodOverride = require('method-override');
 
-// Environment variables
-require('dotenv').config();
+const methodOverride = require('method-override');
 
 // Application Setup
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Express middleware
-// Utilize ExpressJS functionality to parse the body of the request
+// Application Middleware
 app.use(express.urlencoded({ extended: true }));
-// Specify a directory for static resources
-app.use(express.static('./public'));
+app.use(methodOverride('_method'));
 
-// Middleware to handle PUT and DELETE
-app.use(methodOverride((request, response) => {
-  if (request.body && typeof request.body === 'object' && '_method' in request.body) {
-    // look in urlencoded POST bodies and delete it
-    let method = request.body._method;
-    delete request.body._method;
-    return method;
-  }
-}))
+// Set the view engine for server-side templating
+app.set('view engine', 'ejs');
+
+// Static Routes
+app.use(express.static('public'));
 
 // Database Setup
 const client = new pg.Client(process.env.DATABASE_URL);
-client.connect();
 client.on('error', err => console.error(err));
 
 // Set the view engine for server-side templating
@@ -38,71 +32,86 @@ app.set('view engine', 'ejs');
 
 // API Routes
 app.get('/', getTasks);
-
 app.get('/tasks/:task_id', getOneTask);
-
 app.get('/add', showForm);
-
 app.post('/add', addTask);
-
 app.put('/update/:task_id', updateTask);
+app.delete('/delete/:task_id', deleteTask);
 
+
+
+// Failsafe Routes
 app.get('*', (req, res) => res.status(404).send('This route does not exist'));
-
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+app.get((error, req, res) => handleError(error, res)); // handle errors
 
 
 // HELPER FUNCTIONS
 
-function getTasks(request, response) {
+function getTasks(req, res) {
   let SQL = 'SELECT * from tasks;';
 
   return client.query(SQL)
-    .then(results => response.render('index', { results: results.rows }))
+    .then(results => res.render('index', { results: results.rows }))
     .catch(handleError);
 }
 
-function getOneTask(request, response) {
+function getOneTask(req, res) {
   let SQL = 'SELECT * FROM tasks WHERE id=$1;';
-  let values = [request.params.task_id];
+  let values = [req.params.task_id];
 
   return client.query(SQL, values)
     .then(result => {
       // console.log('single', result.rows[0]);
-      return response.render('pages/detail-view', { task: result.rows[0] });
+      return res.render('pages/detail-view', { task: result.rows[0] });
     })
-    .catch(err => handleError(err, response));
+    .catch(err => handleError(err, res));
 }
 
-function showForm(request, response) {
-  response.render('pages/add-view');
+function showForm(req, res) {
+  res.render('pages/add-view');
 }
 
-function addTask(request, response) {
-  console.log(request.body);
-  let { title, description, category, contact, status } = request.body;
+function addTask(req, res) {
+  // console.log(req.body);
+  let { title, description, category, contact, status } = req.body;
 
   let SQL = 'INSERT INTO tasks(title, description, category, contact, status) VALUES ($1, $2, $3, $4, $5);';
   let values = [title, description, category, contact, status];
 
   return client.query(SQL, values)
-    .then(response.redirect('/'))
-    .catch(err => handleError(err, response));
+    .then(res.redirect('/'))
+    .catch(err => handleError(err, res));
 }
 
-function updateTask(request, response) {
-  // destructure variables
-  let { title, description, category, contact, status } = request.body;
-  // need SQL to update the specific task that we were on
-  let SQL = `UPDATE tasks SET title=$1, description=$2, category=$3, contact=$4, status=$5 WHERE id=$6;`;
-  // use request.params.task_id === whatever task we were on
-  let values = [title, description, category, contact, status, request.params.task_id];
+
+// app.put('/update/:task_id', updateTask);
+
+function updateTask(req, res){
+  // console.log(req);
+  let { title, description, category, contact, status } = req.body;
+  let SQL = 'UPDATE tasks SET title=$1, description=$2, contact=$3, category=$4, status=$5 WHERE id=$6;';
+  let values = [title, description, contact, category, status, req.params.task_id];
 
   client.query(SQL, values)
-    .then(response.redirect(`/tasks/${request.params.task_id}`))
-    .catch(err => handleError(err, response));
+  .then(res.redirect(`/tasks/${req.params.task_id}` ))
+  .catch(err => handleError(err, res));
 }
 
-function handleError(error, response) {
-  response.render('pages/error-view', { error: 'Uh Oh' });
+
+function deleteTask(req, res){
+  let SQL  = 'DELETE FROM tasks WHERE id=$1';
+  let values = [req.params.task_id];
+  client.query(SQL, values)
+  .then(res.redirect('/'))
+  .catch(err => handleError(err, res));
 }
+
+
+function handleError(error, res) {
+  res.render('pages/error-view', { error: 'Uh Oh' });
+}
+
+client.connect()
+  .then(() => {
+    app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+  });
